958c8292f3d8176876fb5190781228b3653a43ac
shivaco <ezboost322@gmail.com>
2019-10-04 01:08:20 +0600

    Version bump

--- dbl/__init__.py
+++ dbl/__init__.py
@@ -12,7 +12,7 @@ __title__ = 'dblpy'
 __author__ = 'Francis Taylor'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2019 Assanali Mukhanov'
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 from collections import namedtuple
 
