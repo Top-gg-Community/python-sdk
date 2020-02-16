3e663a66d02eebbd70fc697e0ebec86c632192e7
Gavyn S <38404553+A-Trash-Coder@users.noreply.github.com>
2020-02-16 17:14:43 -0500

    Update errors.py

--- dbl/errors.py
+++ dbl/errors.py
@@ -3,7 +3,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2019 Assanali Mukhanov
+Copyright (c) 2020 Assanali Mukhanov
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
