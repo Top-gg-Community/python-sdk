472d3b7d92268a14c24719d05242a50211191119
Gavyn S <38404553+A-Trash-Coder@users.noreply.github.com>
2020-02-16 17:16:25 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'dblpy'
-copyright = '2019, Assanali Mukhanov'
+copyright = '2020, Assanali Mukhanov'
 author = 'Assanali Mukhanov'
 
 # The version info for the project you're documenting, acts as replacement for
