fb4a36b8a967696664e3531705a0d0c725ae9e64
shivaco <22858781+shivaco@users.noreply.github.com>
2020-08-16 02:42:05 +0600

    "DBL" -> "top.gg"

--- dbl/http.py
+++ dbl/http.py
@@ -54,7 +54,7 @@ async def _json_or_text(response: ClientResponse) -> Union[dict, str]:
 
 
 class HTTPClient:
-    """Represents an HTTP client sending HTTP requests to the DBL API.
+    """Represents an HTTP client sending HTTP requests to the top.gg API.
 
     .. _aiohttp session: https://aiohttp.readthedocs.io/en/stable/client_reference.html#client-session
 
@@ -75,25 +75,25 @@ class HTTPClient:
         self._global_over = asyncio.Event(loop=self.loop)
         self._global_over.set()
 
-        user_agent = 'DBL-Python-Library (https://github.com/DiscordBotList/DBL-Python-Library {0}) Python/{1[0]}.{1[' \
+        user_agent = 'DBL-Python-Library (https://github.com/top-gg/DBL-Python-Library {0}) Python/{1[0]}.{1[' \
                      '1]} aiohttp/{2}'
         self.user_agent = user_agent.format(__version__, sys.version_info, aiohttp.__version__)
 
     # NOTE: current implementation doesn't maintain state over restart
 
     async def request(self, method, url, **kwargs) -> Union[dict, str]:
-        """Handles requests to the API"""
+        """Handles requests to the API."""
         url = "{0}{1}".format(self.BASE, url)
         rate_limiter = RateLimiter(max_calls=59, period=60, callback=_ratelimit_handler)
         # handles rate limits.
         # max_calls is set to 59 because current implementation will retry in 60s
-        # after 60 calls is reached. DBL has a 1h block so obviously this doesn't work well,
+        # after 60 calls is reached. top.gg has a 1h block so obviously this doesn't work well,
         # as it will get a 429 when 60 is reached.
 
-        async with rate_limiter:  # this works but doesn't 'save' over restart. need a better implementation.
+        async with rate_limiter:  # this works but doesn't 'save' over restart.
 
             if not self.token:
-                raise errors.UnauthorizedDetected("DBL token not provided")
+                raise errors.UnauthorizedDetected("top.gg API token not provided")
 
             headers = {
                 'User-Agent'   : self.user_agent,
@@ -161,7 +161,7 @@ class HTTPClient:
     #     self.session = aiohttp.ClientSession(loop=self.session.loop)
 
     async def post_guild_count(self, bot_id, guild_count, shard_count, shard_no):
-        """Posts bot's guild count and shards info on DBL"""
+        """Posts bot's guild count and shards info on top.gg."""
         if shard_count:
             payload = {
                 'server_count': guild_count,
@@ -175,15 +175,15 @@ class HTTPClient:
         await self.request('POST', '/bots/{}/stats'.format(bot_id), json=payload)
 
     async def get_weekend_status(self):
-        """Gets the weekend status from DBL"""
+        """Gets the weekend status from top.gg."""
         return await self.request('GET', '/weekend')
 
     async def get_guild_count(self, bot_id):
-        """Gets the guild count of the given Bot ID"""
+        """Gets the guild count of the given Bot ID."""
         return await self.request('GET', '/bots/{}/stats'.format(bot_id))
 
     async def get_bot_info(self, bot_id):
-        """Gets the information of the given Bot ID"""
+        """Gets the information of a bot under given bot ID on top.gg."""
         resp: Union[dict, str] = await self.request('GET', '/bots/{}'.format(bot_id))
         resp['date'] = datetime.strptime(resp['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
         for k in resp:
@@ -192,11 +192,11 @@ class HTTPClient:
         return resp
 
     async def get_bot_upvotes(self, bot_id):
-        """Gets your bot's last 1000 votes on DBL"""
+        """Gets your bot's last 1000 votes on top.gg."""
         return await self.request('GET', '/bots/{}/votes'.format(bot_id))
 
     async def get_bots(self, limit, offset, sort, search, fields):
-        """Gets an object of bots on DBL"""
+        """Gets an object of bots on top.gg."""
         if limit > 500:
             limit = 50
         fields = ', '.join(fields)
@@ -207,16 +207,16 @@ class HTTPClient:
         })
 
     async def get_user_info(self, user_id):
-        """Gets an object of the user on DBL"""
+        """Gets an object of the user on top.gg."""
         return await self.request('GET', '/users/{}'.format(user_id))
 
     async def get_user_vote(self, bot_id, user_id):
-        """Gets an info whether the user has upvoted your bot"""
+        """Gets info whether the user has upvoted your bot."""
         return await self.request('GET', '/bots/{}/check'.format(bot_id), params={'userId': user_id})
 
 
 async def _ratelimit_handler(until):
-    """Handles the message shown when we are ratelimited"""
+    """Handles the displayed message when we are ratelimited."""
     duration = round(until - datetime.now().timestamp())
     mins = duration / 60
     fmt = 'We have exhausted a ratelimit quota. Retrying in %.2f seconds (%.3f minutes).'
