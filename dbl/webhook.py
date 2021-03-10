a4bcde4ea457f1c62835a2828b255b543ba04325
shivaco <22858781+shivaco@users.noreply.github.com>
2021-03-10 22:13:59 +0600

    Helper methods documented and allowed custom routes

--- dbl/webhook.py
+++ dbl/webhook.py
@@ -36,11 +36,10 @@ log = logging.getLogger(__name__)
 
 class WebhookManager:
     """
-    This class is used as a manager for top.gg webhooks.
+    This class is used as a manager for the top.gg webhook.
 
     Parameters
     ----------
-
     bot: discord.Client
         The Client object that will be utilized by this manager's webhook(s) to emit events.
     """
@@ -55,19 +54,37 @@ class WebhookManager:
         self.__app = web.Application()
         self._is_closed = False
 
-    def dbl_webhook(self, path: str, auth_key: str):
+    def dbl_webhook(self, route: str, auth_key: str):
+        """Configure and activate a route that listens to bot votes.
+
+        Parameters
+        ----------
+        route: str
+            The route to use for bot votes.
+        auth_key: str
+            The Authorization key that will be used to verify the incoming requests.
+        """
         self._webhooks["dbl"] = {
-            "path": path or "/dbl",
-            "auth": auth_key or "",
-            "func": self._bot_vote_handler
+            "route": route or "/dbl",
+            "auth" : auth_key or "",
+            "func" : self._bot_vote_handler
         }
         return self
 
-    def dsl_webhook(self, path: str, auth_key: str):
+    def dsl_webhook(self, route: str, auth_key: str):
+        """Configure and activate a route that listens to server votes.
+
+        Parameters
+        ----------
+        route: str
+            The route to use for server votes.
+        auth_key: str
+            The Authorization key that will be used to verify the incoming requests.
+        """
         self._webhooks["dsl"] = {
-            "path": path or "/dsl",
-            "auth": auth_key or "",
-            "func": self._guild_vote_handler
+            "route": route or "/dsl",
+            "auth" : auth_key or "",
+            "func" : self._guild_vote_handler
         }
         return self
 
@@ -88,18 +105,25 @@ class WebhookManager:
         return web.Response(status=401, text="Unauthorized")
 
     async def run(self, port: int):
+        """Run the webhook.
+
+        Parameters
+        ----------
+        port: int
+            The port to run the webhook on.
+        """
         for webhook in self._webhooks:
-            self.__app.router.add_post(self._webhooks[webhook]["path"], self._webhooks[webhook]["func"])
+            self.__app.router.add_post(self._webhooks[webhook]["route"], self._webhooks[webhook]["func"])
         runner = web.AppRunner(self.__app)
         await runner.setup()
         self._webserver = web.TCPSite(runner, '0.0.0.0', port)
         await self._webserver.start()
         self._is_closed = False
-        
+
     @property
     def webserver(self) -> Optional[web.TCPSite]:
         """Returns the internal webserver that handles webhook requests.
-        
+
         Returns
         --------
         Optional[:class:`aiohttp.web.TCPSite`]
@@ -108,5 +132,6 @@ class WebhookManager:
         return getattr(self, '_webserver', None)
 
     async def close(self):
+        """Stop the webhook."""
         await self._webserver.stop()
         self._is_closed = True
