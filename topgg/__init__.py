a67e769e71eb04485f1b57722e11003203262376
shivaco <22858781+shivaco@users.noreply.github.com>
2021-03-28 17:39:50 +0600

    Fix circular imports

--- topgg/__init__.py
+++ topgg/__init__.py
@@ -9,16 +9,16 @@ A basic wrapper for the Top.gg API.
 """
 from collections import namedtuple
 
-from .client import DBLClient
-from .errors import *
-from .http import HTTPClient
-from .webhook import WebhookManager
 
 __title__ = 'topggpy'
 __author__ = 'Assanali Mukhanov'
 __license__ = 'MIT'
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-
 version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel='final', serial=0)
 
 __version__ = f'{version_info.major}.{version_info.minor}.{version_info.micro}'
+
+from .client import DBLClient
+from .http import HTTPClient
+from .errors import *
+from .webhook import WebhookManager
