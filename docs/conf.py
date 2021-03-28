6f2d241d3737939168e95a6618c3fb0bbb99b946
shivaco <22858781+shivaco@users.noreply.github.com>
2021-03-28 21:27:57 +0600

    Nothing important here

--- docs/conf.py
+++ docs/conf.py
@@ -17,13 +17,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
 import os
-# import re
 import sys
 
 sys.path.insert(0, os.path.abspath('../'))
-
 from topgg import __version__ as version
+
+# import re
+
 
 # -- General configuration ------------------------------------------------
 
