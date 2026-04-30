5746320f84c793fbb8b8ae3126e38454548381cf
null <60427892+null8626@users.noreply.github.com>
2026-04-30 10:37:26 +0700

    fix: fix parse_timestamp for Python <= 3.10

--- topgg/util.py
+++ topgg/util.py
@@ -1,18 +1,21 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2026 null8626 & Top.gg
+
 from datetime import datetime
 from sys import version_info
 
 
-if version_info.major == 3 and version_info.minor <= 10:
+if version_info.major == 3 and version_info.minor <= 10:  # pragma: nocover
   from re import compile
 
-  TIMESTAMP_MILLISECOND_FIX_REGEX = compile(r'\.(\d+)')
-  TIMESTAMP_MILLISECOND_FIX_TRIMMER = lambda match: match.group(1)[:3]
+  TIMESTAMP_MILLISECOND_FIX_REGEX = compile(r'\.(\d{4,})')
+  TIMESTAMP_MILLISECOND_FIX_TRIMMER = lambda match: f'.{match.group(1)[:3]}'
 
 
 def parse_timestamp(timestamp: str) -> datetime:
   """Parses an ISO format timestamp to a Python datetime instance."""
 
-  if version_info.major == 3 and version_info.minor <= 10:
+  if version_info.major == 3 and version_info.minor <= 10:  # pragma: nocover
     timestamp = TIMESTAMP_MILLISECOND_FIX_REGEX.sub(
       TIMESTAMP_MILLISECOND_FIX_TRIMMER, timestamp
     )
